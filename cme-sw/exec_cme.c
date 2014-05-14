957be2c580cc7e343af0933ad44e76f34aefcbd2
vidhatreg <gathey.vidhatre@gmail.com>
2014-05-14 13:03:36 -0400

    updated exec_cme with time anaylsis, need to be tested on board, haven't compiled yet

--- cme-sw/exec_cme.c
+++ cme-sw/exec_cme.c
@@ -1,6 +1,8 @@
 /*
- * Userspace program that communicates with the span_cme peripheral
- * primarily through ioctls
+ * -Userspace program that communicates with the span_cme peripheral
+ *  primarily through ioctls
+ * -program reads the portifolio data from the "input_file.txt"
+ * -program outputs all the read data from the peripheral to "output_file.txt"
  *
  * Vidhatre Gathey
  * Columbia University
@@ -15,10 +17,13 @@
 #include <string.h>
 #include <unistd.h>
 #include <inttypes.h>
-#define NUM_OF_PORTIFOLIO 3
+#define NUM_OF_PORTIFOLIO 3 // number of portifolios the input contains
 int span_cme_fd;
 
-/* Read and print the segment values */
+/* function to read the output from the board using an ioctl function, 
+ * open an output file, and 
+ * append the output data to the file 
+ */
 void print_output() {
     portifolio_t port;
     if (ioctl(span_cme_fd, SPAN_CME_READ_DIGIT, &port)) {
@@ -30,11 +35,12 @@ void print_output() {
     fpout = fopen("output_file.txt","a"); 
     fprintf(fpout,"%04d \n", port.output);
     if (fpout) fclose(fpout);
-  //}
   printf("\n");
 }
 
-/* Write the contents of the array to the display */
+/* Write the contents of the input portifolio data to the board, 
+ * used the ioctl function to wrtie to the board
+ */
 void write_portifolio(short var[])
 {
   portifolio_t port;
@@ -50,32 +56,39 @@ void write_portifolio(short var[])
 
 int main()
 {
-  portifolio_t port;
-  int i,j,k;
-  short var[DATA_LENGTH];
-  static const char filename[] = "/dev/span_cme";
+  portifolio_t port;		// portifolio template as described in the header span_cme.h
+  int i,j,k;			// iterators
+  short var[DATA_LENGTH];       // used as a buffer for storing the data from the input file as it is read
+  char label[DATA_LENGTH][14];  // store the first line of labels from the input file
+  static const char filename[] = "/dev/span_cme"; 
+  clock_t begin_time, end_time;
   FILE *fpin;
   fpin=fopen("input_file.txt", "r+");
-  //FILE *fpout;
-  //fpout=fopen("output_file.txt", "a");
-  
-  printf("SPAN CME Userspace program started\n");
 
+  printf("SPAN CME Userspace program started\n");
+  begin_time = clock();
   if ( (span_cme_fd = open(filename, O_RDWR)) == -1) {
     fprintf(stderr, "could not open %s\n", filename);
     return -1;
   }
+  
+  for(k = 0; k < DATA_LENGTH; k++)		// read portifolio labels
+  	fscanf( fpin, "%s",&label[k]);
 
-  for (j = NUM_OF_PORTIFOLIO ; j > 0 ; j--){
+  for (j = NUM_OF_PORTIFOLIO ; j > 0 ; j--){   // read the first set of data i.e. from the portifolio
   	for(k = 0; k < DATA_LENGTH; k++) 
   		fscanf( fpin, "%hu",&var[k]);
   	//printf( " inputs: %hu, %hu ",var0,var1);
   	write_portifolio(var);
   	print_output();
+	// debug: outputs
   	printf(" done : %d \n", j);
   }
   fclose(fpin);
   //fclose(fpout);
+  end_time = clock();
+
+  printf("total execution time: %f \n", (double) (end_time - begin_time)/CLOCKS_PER_SEC );
   printf("SPAN CME Userspace program terminating\n");
   return 0;
 }
